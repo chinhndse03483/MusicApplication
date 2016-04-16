6fd46154841333fa0caec8019a24ce99b5a3960a
ichnv <ichnv2610@gmail.com>
2016-04-16 13:30:45 +0700

    google add

---
+++ Hackathon_ios_gen2_Zing/GoogleAdsViewController.h
@@ -0,0 +1,14 @@
+//
+//  GoogleAdsViewController.h
+//  Hackathon_ios_gen2_Zing
+//
+//  Created by IchIT on 4/16/16.
+//  Copyright © 2016 Lê Tuấn Anh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import GoogleMobileAds;
+@interface GoogleAdsViewController : UIViewController
+@property (strong, nonatomic) IBOutlet GADBannerView *bannerView;
+
+@end
